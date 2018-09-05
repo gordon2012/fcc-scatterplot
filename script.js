14f3743e759095f7e4b819df80a104a2629afc32
Gordon Doskas <gordon2012@users.noreply.github.com>
2018-09-04 21:27:40 -0700

    create scatterplot WIP
    Signed-off-by: Gordon Doskas <gordon2012@users.noreply.github.com>

--- script.js
+++ script.js
@@ -4,7 +4,12 @@ const w = 800;
 const h = 600;
 const padding = 50;
 
-drawChart = () => {
+// Tooltip
+const tooltip = d3.select('body').append('div')
+    .attr('class', 'tooltip')
+    .style('opacity', 0);
+
+drawGraph = () => {
     const svg = d3.select('.svg-target')
         .html('')
         .append('svg')
@@ -13,13 +18,46 @@ drawChart = () => {
         .attr('height', h)
 
     // x axis
-    // const xScale = d3.scaleLinear()
-    //     .doma
+    const xScale = d3.scaleLinear()
+        .domain([d3.min(data, d => d.Year) - 1, d3.max(data, d => d.Year)])
+        .range([padding, w - padding]);
+
+    const xAxis = d3.axisBottom(xScale)
+        .tickFormat(d3.format('d'));
+
     svg.append('g')
         .attr('transform', `translate(0, ${h - padding})`)
-        .attr('id', 'x-axis');
+        .attr('id', 'x-axis')
+        .call(xAxis);
 
+    // y axis
+    const yScale = d3.scaleLinear()
+        .domain([d3.min(data, d => d.Seconds), d3.max(data, d => d.Seconds)])
+        .range([h - padding, padding]);
 
+    const yAxis = d3.axisLeft(yScale)
+        .tickFormat(d3.format('d'));
+
+    svg.append('g')
+        .attr('transform', `translate(${padding}, 0)`)
+        .attr('id', 'y-axis')
+        .call(yAxis);
+
+    // Dots
+    svg.selectAll('circle')
+        .data(data)
+        .enter()
+        .append('circle')
+            .attr('cx', d => xScale(d.Year))
+            .attr('cy', d => yScale(d.Seconds))
+            .attr('r', 5)
+            .attr('fill', 'red')
+            .on('mouseover', d => {
+                tooltip.transition()
+                    .duration(200)
+                    .style('opacity', 1);
+                tooltip.html(`${d.Year} | ${d.Seconds}`)
+            });
 };
 
 const req = new XMLHttpRequest();
@@ -27,10 +65,6 @@ req.open('GET', 'cyclist-data.json', true);
 req.send();
 req.onload = function() {
     data = JSON.parse(req.responseText);
-
-    // console.log(dataset);
-    // draw
-    drawChart();
-
+    console.log(data);
+    drawGraph();
 };
-
