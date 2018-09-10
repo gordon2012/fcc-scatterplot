8898fa001b66f08f469d39783f49ab2aedb6f291
Gordon Doskas <gordon2012@users.noreply.github.com>
2018-09-09 21:59:18 -0700

    Create scatter plot
    Signed-off-by: Gordon Doskas <gordon2012@users.noreply.github.com>

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const padding = 50;
 
 // Tooltip
 const tooltip = d3.select('body').append('div')
-    .attr('class', 'tooltip')
+    .attr('id', 'tooltip')
     .style('opacity', 0);
 
 const drawGraph = () => {
@@ -31,28 +31,19 @@ const drawGraph = () => {
         .call(xAxis);
 
     // y axis
-    const yScale = d3.scaleLinear()
-        .domain([d3.min(data, d => d.Seconds), d3.max(data, d => d.Seconds)])
+    const parsedTime = data.map(d => d3.timeParse('%M:%S')(d.Time));
+    const yScale = d3.scaleTime()
+        .domain(d3.extent(parsedTime))
         .range([h - padding, padding]);
 
     const yAxis = d3.axisLeft(yScale)
-        .tickFormat(d3.format('d'));
+        .tickValues(parsedTime)
+        .tickFormat((d,i) => data[i].Time);
 
     svg.append('g')
         .attr('transform', `translate(${padding}, 0)`)
         .attr('id', 'y-axis')
         .call(yAxis);
-
-    data.forEach(d => {
-        var date = new Date();
-        date.setTime(d.Seconds * 1000);
-
-        // console.log((new Date()).setTime(d.Seconds * 1000));
-        // console.log(date);
-        // console.log(d.Time);
-        // console.log(d.Time.slice(0,2));
-        // console.log(d.Time.slice(3,5));
-    });
 
     // Dots
     svg.selectAll('circle')
@@ -60,11 +51,10 @@ const drawGraph = () => {
         .enter()
         .append('circle')
             .attr('cx', d => xScale(d.Year))
-            .attr('cy', d => yScale(d.Seconds))
+            .attr('cy', d => yScale(d3.timeParse('%M:%S')(d.Time)))
             .attr('r', 5)
             .attr('fill', 'red')
-            .on('mouseover', d => {
-                // console.log(~~(d.Seconds / 60));
+            .on('mouseover', (d,i,a) => {
                 tooltip.transition()
                     .duration(200)
                     .style('opacity', 1);
@@ -78,7 +68,8 @@ const drawGraph = () => {
                     // TODO: to the left
                     // .style('left', `${d3.event.pageX + ( (w-160-20-d3.event.pageX)>0 ? 20 : -180      )}px`)
                     // .style('right', `${d3.event.pageX}px`)
-                    .style('top', `${d3.event.pageY}px`);
+                    .style('top', `${d3.event.pageY}px`)
+                    .attr('data-year', a[i].dataset.xvalue);
                 })
             .on('mouseout', d => {
                 tooltip.transition()
@@ -87,29 +78,7 @@ const drawGraph = () => {
             })
             .attr('class', 'dot')
             .attr('data-xvalue', d => d.Year)
-            .attr('data-yvalue', d => {
-                // let date = new Date();
-
-                var date1 = (new Date(Date.UTC(1970, 0, 1, 0, parseInt(d.Time.slice(0,2)), parseInt(d.Time.slice(3,5))))).toISOString();
-                // TODO: make it work!
-                // var date2 = (new Date(Date.UTC(1970, 0, 1, 0, parseInt(d.Time.slice(0,2)), parseInt(d.Time.slice(3,5))))).toISOString();
-
-                // console.log(yScale(d.Seconds));
-                return date1;
-
-                // console.log(   (new Date(Date.UTC(1970, 0, 1, 0, parseInt(d.Time.slice(0,2)), parseInt(d.Time.slice(3,5))))).toISOString()   );
-
-                // return (new Date(Date.UTC(1970, 0, 1, 0, parseInt(d.Time.slice(0,2)), parseInt(d.Time.slice(3,5)))));
-
-                // console.log(Date.UTC(1970, 0, 1, 0));
-                // console.log(d.Time.slice(0,2));
-                // console.log(d.Time.slice(3,5));
-
-                // date.setTime(d.Seconds * 1000);
-                // return date;
-                // console.log((new Date()).setTime(d.Seconds * 1000));
-                // console.log(date);
-            })
+            .attr('data-yvalue', d => d3.timeParse('%M:%S')(d.Time));
 };
 
 const req = new XMLHttpRequest();
@@ -117,6 +86,5 @@ req.open('GET', 'cyclist-data.json', true);
 req.send();
 req.onload = function() {
     data = JSON.parse(req.responseText);
-    console.log(data);
     drawGraph();
 };
