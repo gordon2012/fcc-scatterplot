d572ee6fb2b0b28dea438f8c47191c0be9a87c1d
Gordon Doskas <gordon2012@users.noreply.github.com>
2018-09-10 21:58:48 -0700

    Make graph responsive
    Signed-off-by: Gordon Doskas <gordon2012@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,8 +1,52 @@
 let data;
 
-const w = 800;
-const h = 600;
-const padding = 50;
+const maxW = 800;
+const maxH = 600;
+
+const maxPadding = 50;
+const minPadding = 50;
+const wrapPadding = 15;
+
+let w = 800;
+let h = 600;
+let padding = maxPadding;
+
+function debounce(func, wait, immediate) {
+    var timeout;
+    return function() {
+        var context = this, args = arguments;
+        var later = function() {
+            timeout = null;
+            if(!immediate) {
+                func.apply(context, args);
+            }
+        }
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if(callNow) {
+            func.apply(context, args);
+        }
+    };
+};
+
+var resize = debounce(function () {
+    const title = document.getElementById('title');
+    const titleStyle = getComputedStyle(title);
+    const titleHeight = title.offsetHeight + parseInt(titleStyle.marginTop) + parseInt(titleStyle.marginBottom);
+
+    if(window.innerWidth < 900) {
+        w = window.innerWidth - (wrapPadding * 2);
+        h = window.innerHeight - titleHeight - (wrapPadding * 2) - 5;
+        padding = minPadding;
+    } else {
+        w = maxW;
+        h = maxH;
+        padding = maxPadding;
+    }
+
+    drawGraph();
+}, 100);
 
 // Tooltip
 const tooltip = d3.select('body').append('div')
@@ -90,5 +134,9 @@ req.open('GET', 'cyclist-data.json', true);
 req.send();
 req.onload = function() {
     data = JSON.parse(req.responseText);
-    drawGraph();
+    resize();
 };
+
+document.addEventListener('DOMContentLoaded', () => {
+    window.addEventListener('resize', resize);
+});
